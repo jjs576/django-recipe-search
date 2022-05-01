26101a159fb4c0a25d9ea1b269e2b0cd5a59fd54
jjs576 <jjs576@naver.com>
2022-05-02 03:34:02 +0900

    Feat: debug settings, urls

--- recipe_search/settings.py
+++ recipe_search/settings.py
@@ -49,11 +49,19 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Third Apps
     'rest_framework',
+    'debug_toolbar',
     # Local Apps
+    'common',
     'recipe',
 ]
 
+# TODO: split dev/prod
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -139,3 +147,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10,
+}
--- recipe_search/urls.py
+++ recipe_search/urls.py
@@ -20,14 +20,15 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include([
+        path('recipe/', include('recipe.urls')),
+    ])),
 ]
 
 if settings.DEBUG:
-
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-    import debug_toolbar
     urlpatterns += [
-        path('__debug__/', include(debug_toolbar.urls)),
+        path('__debug__/', include('debug_toolbar.urls')),
     ]
